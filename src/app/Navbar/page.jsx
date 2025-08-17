894ccec4d84da7532add966a35d4366d42c8c6b1
rajib777durjoy <rajib777chanda@gmail.com>
2025-08-17 19:36:04 +0600

    globe_vista_agency_Project

---
+++ src/app/Navbar/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+         <ul>
+            <li>home</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+         </ul>
+        </div>
+    );
+};
+
+export default Navbar;
